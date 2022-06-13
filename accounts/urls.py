87e2ca6169ea5176ead2e410b195975e3c2b5a02
Samuel Sackey <smlscky@gmail.com>
2022-06-13 21:49:42 +0000

    Setup register and login templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name="index"),
+    path('login', views.login_view, name="login"),
+    path('register', views.register_view, name="register"),
+    path('logout', views.logout_view, name="logout")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect(reverse("accounts:login"))
+
+    return render(request, "accounts/home.html")
+
+
+def login_view(request):
+    return render(request, "accounts/login.html")
+
+
+def register_view(request):
+    return render(request, "accounts/register.html")
+
+
+def logout_view(request):
     return render(request, "accounts/home.html")
