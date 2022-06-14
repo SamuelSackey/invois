0c72181992e96b370175e530497e93a607f743b7
Samuel Sackey <smlscky@gmail.com>
2022-06-14 06:37:47 +0000

    Setup business model and logic

--- invoice/models.py
+++ invoice/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=128)
+    contact = models.IntegerField()
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    country = models.CharField(max_length=64)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="businesses"
+    )
+
+    def __str__(self):
+        return f"{self.name} | {self.contact} | {self.country} "
--- invoice/views.py
+++ invoice/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
+    has_business = Business.objects.filter(user=request.user)
+
+    if not has_business:
+        return redirect(reverse("invoice:add_business"))
+
     return render(request, "invoice/index.html")
 
 
 def add_business(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        contact = request.POST["contact"]
+        address = request.POST["address"]
+        city = request.POST["city"]
+        country = request.POST["country"]
+
+        business = Business(name=name, contact=contact, address=address,
+                            city=city, country=country, user=request.user)
+        business.save()
+        print(business)
+
+        return redirect(reverse("invoice:index"))
+
     return render(request, "invoice/add_business.html")
