9a824e461ddf9a5fafa8d9e2116a07b076751a26
Samuel Sackey <smlscky@gmail.com>
2022-06-14 07:04:48 +0000

    Add item model

--- invoice/models.py
+++ invoice/models.py
@@ -18,3 +18,31 @@ class Business(models.Model):
 
     def __str__(self):
         return f"{self.name} | {self.contact} | {self.country} "
+
+
+class Invoice(models.Model):
+    title = models.CharField(max_length=128)
+    issue_date = models.DateField()
+    tax = models.IntegerField(default=0)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="invoices"
+    )
+
+    def __str__(self):
+        return f"{self.title} | {self.issue_date} | {self.tax}% "
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=128)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField(default=0)
+    invoice = models.ForeignKey(
+        Invoice,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    def __str__(self):
+        return f"{self.name} | qty: {self.quantity} | ${self.price} "
